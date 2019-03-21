db3bd0a474868f8677af25e9eecc2c8abff33e69
Brian Bycynski <high5bri@hotmail.com>
2019-03-21 18:47:32 -0400

    still working

---
+++ scripts/section-15/174-element-is-in-a-array.js
@@ -0,0 +1,5 @@
+const transport = ['Bus', 'Car', 'Bicycle', 'Airplane'];
+
+console.log(elementFound(transport, 'Bus')); // true
+console.log(elementFound(transport, 'Phone')); // false
+console.log(elementFound(transport, 'Airplane')); // true
