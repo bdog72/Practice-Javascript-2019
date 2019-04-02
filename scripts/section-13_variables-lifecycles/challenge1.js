a25a424c6781b913a9d50e695bf80de1e0508864
Brian Bycynski <high5bri@hotmail.com>
2019-04-02 05:07:54 -0400

    finished lecture 125

---
+++ scripts/section-13_variables-lifecycles/challenge1.js
@@ -0,0 +1,39 @@
+/**
+ * CHALLENGE 1-1 TASK
+ *
+ * Rewrite code below using let, const and var according to
+ * variables usage guidelines
+ */
+
+'use strict';
+
+const numbers1 = [23, 87, 110, 11, 20, 5, 34];
+const numbers2 = [11, 21, 31];
+
+const onlyOddNumbers = function(arr) {
+  var oddNumbers = [];
+  var evenNumbersQuantity = 0;
+  const LEN = arr.length;
+
+  for (let i = 0; i < LEN; i++) {
+    arr[i] % 2 ? oddNumbers.push(arr[i]) : evenNumbersQuantity++;
+  }
+
+  if (evenNumbersQuantity === 0) {
+    let info = 'Array contains only odd numbers';
+    console.log(info);
+  } else {
+    let info = 'There are ' + evenNumbersQuantity + ' even numbers';
+    console.log(info);
+  }
+
+  return {
+    oddNumbers: oddNumbers,
+    EVEN_NUMBERS_QUANTITY: evenNumbersQuantity
+  };
+};
+
+// var EVEN_NUMBERS_QUANTITY;
+
+console.log(onlyOddNumbers(numbers1));
+console.log(onlyOddNumbers(numbers2));
